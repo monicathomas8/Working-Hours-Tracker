5a55f4e3d46dc1532bf72c0eb1006e1cb05725fb
Monica Thomas <monicathomas8@icloud.com>
2024-08-18 15:52:23 +0000

    added function to collect and check date of shift

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from datetime import datetime
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,12 +14,24 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Working-Hours-Tracker-PP3')
 
 """
-this code above was learned from the loveSandwiches project
+the code above was learned from the loveSandwiches project
 To install and work with googlesheets.
 """
+#hours = SHEET.worksheet('hours')
 
-hours = SHEET.worksheet('hours')
+def get_shift_date(collect_date):
+    """
+    collects the date of the shift worked and checks it the date is
+    in the correct format
+    """
+collect_date = input("Please enter the date of your shift DD/MM/YYYY): \n")
+print("Checking data...\n")
 
-data = hours.get_all_values()
+try:
+    shift_date = datetime.strptime(collect_date, '%d/%m/%Y').date()
+    print(f"Date entered: {shift_date}")
+except ValueError as e:
+  
+    print("Please enter the date in DD/MM/YYYY format to continue.\n")
 
-print(data)
+get_shift_date(collect_date)
