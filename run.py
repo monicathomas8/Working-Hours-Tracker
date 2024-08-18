d27ca56c62aa07425d54c0ac222bab2e87b3ae38
Monica Thomas <monicathomas8@icloud.com>
2024-08-18 17:06:29 +0000

    added functions to update googlesheet and correct errors

--- run.py
+++ run.py
@@ -12,40 +12,58 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Working-Hours-Tracker-PP3')
-
 """
 the code above was learned from the loveSandwiches project
 To install and work with googlesheets.
 """
-#hours = SHEET.worksheet('hours')
+
+shift_date = None
 
 def get_shift_date():
     """
-    collects the date of the shift worked 
+    collects the date of the shift worked and checks it is valid 
+    with the validate_date function the loop will continue until 
+    valid date is entered.
     """
+    global shift_date
     while True:
         collect_date = input("Please enter the date of your shift DD/MM/YYYY): \n")
         print("Checking data...\n")
 
-        if validate_date(collect_date):
+        shift_date = validate_date(collect_date)
+        if shift_date:
             print("Date is correct")
             break
 
 
 def validate_date(collect_date):
     """
-    Checks if the input is in the correct format
+    Checks if the input is in the correct format and returns the 
+    parsed date if valid, otherwise retuen None.
     """
     try:
         # attepmt to parse the date into the correct format.
-        shift_date = datetime.strptime(collect_date, '%d/%m/%Y').date()
-        print(f"Date entered: {shift_date}")
-    #add code to update time column in the google sheet.
+        parsed_date = datetime.strptime(collect_date, '%d/%m/%Y').date()
+        print(f"Date entered: {parsed_date}")
+        return parsed_date
+
     except ValueError as e:
         # If the date is not valid, print an error message and return false
         print("Please enter the date in DD/MM/YYYY format to continue.\n")
-        return False
+        return None
 
-    return True
-# call the function to start trher process
+def update_hours_worksheet():
+    """
+    updates the google worksheet with the user's data.
+    """
+    print("Updating Hours worksheet...\n")
+    hours_worksheet = SHEET.worksheet("hours")
+    hours_worksheet.append_row([shift_date.strftime('%d/%m/%Y')])
+    print("Date added to Hours worksheet.\n")
+
+#  call the function to start the process
 get_shift_date()
+
+#checks if shift date was correctly set
+if shift_date:
+    update_hours_worksheet()
